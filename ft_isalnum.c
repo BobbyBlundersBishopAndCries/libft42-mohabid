cfa00b79a43f07427079ee705a05c4e5a8b63f0d
medabid <med080abid@gmail.com>
2024-08-31 14:55:49 +0200

    Remove all files from repository

--- ft_isalnum.c
+++
@@ -1,5 +0,0 @@
-#include "libft.h"
-int	ft_isalnum(int c)
-{
-	return ((c <= 'z' && c >= 'a') || (c >= 'A' && c <= 'Z') || (c <= '9' && c >= '0'));
-}
--- ft_putchar_fd.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-#include <unistd.h>
-
-void ft_putchar(char c, int fd)
-{
-	write (fd, &c, 1);
-}
